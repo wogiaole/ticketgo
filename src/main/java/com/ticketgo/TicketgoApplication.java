a489af8e4a5ae31023171e1e44a745286ad84b1d
tang <tanglf999@outlook.com>
2023-11-05 23:28:43 +0000

    第一次提交

--- src/main/java/com/ticketgo/TicketgoApplication.java
+++ src/main/java/com/ticketgo/TicketgoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
